a366c56ac7da93c576e5e1297f11312bccefcca8
Ridge42 <168596694+Ridge42@users.noreply.github.com>
2024-05-01 15:43:27 -0600

    Update prod.cpp

--- Unit01-Intro-to-cpp/homework0-introduction/prod.cpp
+++ Unit01-Intro-to-cpp/homework0-introduction/prod.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using std::cout, std::endl, std::cerr;
+
+#include <cstdlib>
+using std::strtod;
+
+int main(int argc, char const* argv[]) {
+    char* end;
+
+    if (argc > 2) {
+        double num1 = strtod(argv[1], &end);
+        double num2 = strtod(argv[2], &end);
+        cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+    }
+    else {
+        cerr << "prod requires 2 arguments" << endl;
+        return 1;
+    }
+
+    return 0;
+}
 //  :)
