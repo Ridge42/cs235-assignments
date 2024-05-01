b264e4016edbbd293b4b5e034c16bb8861da0a06
Ridge42 <168596694+Ridge42@users.noreply.github.com>
2024-05-01 16:04:19 -0600

    Update numbers_divisible_by.cpp

--- Unit01-Intro-to-cpp/homework1a-cpp-basics/numbers_divisible_by.cpp
+++ Unit01-Intro-to-cpp/homework1a-cpp-basics/numbers_divisible_by.cpp
@@ -1,2 +1,29 @@
-// :D
+#include <iostream>
+using std::cout, std::endl, std::cerr;
+
+#include <cstdlib>
+
+int main(int argc, char const* argv[]) {
+
+    int start = atoi(argv[1]);
+    int end = atoi(argv[2]);
+    int divisor = atoi(argv[3]);
+
+    if (start < end) {
+        for (;end >= start; start++) {
+            if (start % divisor == 0) {
+                cout << start << endl;
+            }
+        }
+    }
+    else if (start >= end) {
+        for (; start >= end; end++) {
+            if (end % divisor == 0) {
+                cout << end << endl;
+            }
+        }
+    }
+
+    return 0;
+}
 
